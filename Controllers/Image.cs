4bf577f65494c6772a0ce0bcbc55424d34f51374
viktorradu <v.radu@outlook.com>
2024-11-11 21:00:38 -0500

    cleanup

--- Controllers/Image.cs
+++ Controllers/Image.cs
@@ -3,12 +3,12 @@
 namespace cherished_server.Services
 {
     [ApiController]
-    public class Image
+    public class Image : ControllerBase
     {
         [HttpGet]
         public IActionResult GetImage([FromQuery] string size, [FromQuery] int key)
         {
-            var image = System.IO.File.OpenRead(imagePath); 
-            return new File(image, "image/jpeg"); }
+            var image = System.IO.File.OpenRead("F:\\Data\\Pictures\\2024\\Halloween\\FB_IMG_1728821589567.jpg"); 
+            return File(image, "image/jpeg"); }
         }
 }
